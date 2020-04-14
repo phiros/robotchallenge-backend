82e1bff4e78757c4f9129467e1a9659ee0bf91cb
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2020-04-14 09:02:54 +0200

    Adds robotposition post route

--- src/main/java/com/github/phiros/robotchallenge/backend/RobotPathController.java
+++ src/main/java/com/github/phiros/robotchallenge/backend/RobotPathController.java
@@ -2,11 +2,17 @@ package com.github.phiros.robotchallenge.backend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RobotPathController {
     @GetMapping(path = "/")
     public String form() {
         return "form";
+    }
+
+    @PostMapping(path = "/robotposition")
+    public String robotPosition() {
+        return "robotposition";
     }
 }
--- src/test/java/com/github/phiros/robotchallenge/backend/RobotPathControllerTest.java
+++ src/test/java/com/github/phiros/robotchallenge/backend/RobotPathControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,5 +35,25 @@ public class RobotPathControllerTest {
                 .andReturn();
 
         assertThat(result.getResponse().getContentAsString()).contains("<form");
+    }
+
+    @Test
+    public void testRobotPositionRouteAcceptsSubmittedScript() throws Exception {
+        var predefinedScript = String.join(System.getProperty("line.separator"),
+                "POSITION 1 3 EAST //sets the initial position for the robot",
+                "FORWARD 3 //lets the robot do 3 steps forward",
+                "WAIT //lets the robot do nothing",
+                "TURNAROUND //lets the robot turn around",
+                "FORWARD 1 //lets the robot do 1 step forward",
+                "RIGHT //lets the robot turn right",
+                "FORWARD 2 //lets the robot do 2 steps forward"
+        );
+
+        var result = mockMvc.perform(
+                post("/robotposition")
+                        .param("script", predefinedScript))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andReturn();
     }
 }
