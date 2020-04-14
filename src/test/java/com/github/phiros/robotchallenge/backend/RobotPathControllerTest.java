6a326093aea56462460432def59d646eb807d7fe
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2020-04-14 08:44:45 +0200

    Adds html form with example script

--- src/test/java/com/github/phiros/robotchallenge/backend/RobotPathControllerTest.java
+++ src/test/java/com/github/phiros/robotchallenge/backend/RobotPathControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,5 +23,16 @@ public class RobotPathControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andReturn();
+    }
+
+    @Test
+    public void testRootRouteDeliversSiteWithFormExists() throws Exception {
+        var result = mockMvc.perform(
+                get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).contains("<form");
     }
 }
